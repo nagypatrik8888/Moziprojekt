edf98a6463b47b5730586cdadf03821a714988a5
nagypatrik8888 <nagypatrik8888@gmail.com>
2026-02-20 18:52:18 +0100

    ticket orders store wip

--- cinemax/app/Http/Controllers/TicketOrdersController.php
+++ cinemax/app/Http/Controllers/TicketOrdersController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TicketOrdersController extends Controller
 {
-    public function store(){
+    public function store(Request $request){
         $response_for_frontend=[];
-        return response()->json($response_for_frontend);
+
+        $validator = Validator::make($request->all(),[
+            'movie_id' => 'required|integer',
+            'screening_id' => 'required|integer',
+            'seats.*.seat_id' => 'required|integer',
+            'seats.*.price_id' => 'required|integer',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $validated = $validator->valid();
+
+        //dd($validated);
+                              
+        return response()->json($validated);
     }
 }
