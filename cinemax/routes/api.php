f87390a3db11d268985729abcc19ad940c0645ca
nagypatrik8888 <nagypatrik8888@gmail.com>
2026-02-24 19:33:51 +0100

    remove api middleware

--- cinemax/routes/api.php
+++ cinemax/routes/api.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/movies', [MovieController::class, 'index'])->middleware(['auth:sanctum']); ;
+Route::get('/movies', [MovieController::class, 'index']);
 Route::get('/movies/{movie_id}', [MovieController::class, 'show']); //letrehoztunk egy routeot ami fogad egy parametert, hogy a route celja hogy egy film adatait megjelenitsuk
 Route::post('/ticket_orders', [TicketOrdersController::class, 'store']);
-Route::get('/ticket_orders', [TicketOrdersController::class, 'index'])->middleware(['auth:sanctum']); //TODO middleware, hogy csak admin nezhesse
+Route::get('/ticket_orders', [TicketOrdersController::class, 'index']); //TODO middleware, hogy csak admin nezhesse
 Route::get('/profile/ticket_orders', [ProfileTicketOrdersController::class, 'index']);
