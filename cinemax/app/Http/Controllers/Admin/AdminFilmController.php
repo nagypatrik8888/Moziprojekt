1303aed507738ecae8a577d8bda79d71a2559811
nagypatrik8888 <nagypatrik8888@gmail.com>
2026-02-26 17:36:59 +0100

    admin film store fixes

--- cinemax/app/Http/Controllers/Admin/AdminFilmController.php
+++ cinemax/app/Http/Controllers/Admin/AdminFilmController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AdminFilmController extends Controller
 {
@@ -37,25 +38,35 @@ class AdminFilmController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $validator = Validator::make($request->all(), [
             'title' => ['required', 'string', 'max:255'],
             'genre_id' => ['required', 'integer', 'exists:genres,id'],
+            'release_date' => ['required', 'date'],
+            'rating' => ['required', 'numeric'],
+            'duration_min' => ['required', 'integer', 'min:1'],
+            'description' => ['required', 'string'],
+            'language' => ['required', 'string'],
+            'language_id' => ['required', 'integer', 'exists:languages,id'],
+            'poster_url' => ['required', 'string','url:http,https']
 
-            // ezek opcionálisak (ha nálad kötelező valamelyik, szigorítjuk)
-            'release_date' => ['nullable', 'date'],
-            'rating' => ['nullable', 'numeric'],
-            'duration_min' => ['nullable', 'integer', 'min:1'],
-            'description' => ['nullable', 'string'],
         ]);
+         if ($validator->fails()) {
+            return response()->json($validator->errors()); //ha fail json error
+        }
+
+        $validated = $validator->valid();  //ellenorizzuk letezik e a screening
 
         $movie = new Movie();
         $movie->title = $validated['title'];
         $movie->genre_id = $validated['genre_id'];
-        $movie->release_date = $validated['release_date'] ?? null;
+        $movie->release_date = $validated['release_date'];
         $movie->rating = $validated['rating'] ?? null;
-        $movie->duration_min = $validated['duration_min'] ?? null;
-        $movie->description = $validated['description'] ?? null;
-        $movie->save();
+        $movie->duration_min = $validated['duration_min'];
+        $movie->description = $validated['description'];
+        $movie->language = $validated['language'];
+        $movie->language_id = $validated['language_id'];
+        $movie->poster_url = $validated['poster_url'];
+        $movie->save(); //save menti el DB-be
 
         return response()->json([
             'message' => 'Film created',
