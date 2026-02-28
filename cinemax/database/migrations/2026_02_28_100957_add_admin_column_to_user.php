fd334c3398fa09329211225953710cd66e6d4a06
nagypatrik8888 <nagypatrik8888@gmail.com>
2026-02-28 11:28:06 +0100

    user type added

---
+++ cinemax/database/migrations/2026_02_28_100957_add_admin_column_to_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->smallInteger('user_type')->default(1); //1 = sima user
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user_type');
+            //
+        });
+    }
+};
